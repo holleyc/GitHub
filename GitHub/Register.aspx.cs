3cefb621895bd1d3f3e4985e11bea358d7b4306b
Christopher Holley <holleyc7@me.com>
2018-10-01 21:50:00 -0400

    Revert "Installed Microsoft.Owin.Host.SystemWeb NuGet package"
    This reverts commit 2fe84eb317415521479fdd831949502cf0eea8e5.

--- GitHub/Register.aspx.cs
+++ GitHub/Register.aspx.cs
@@ -1,29 +1,17 @@
-﻿using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
-using System;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
 
-namespace WebFormsIdentity
+namespace GitHub
 {
     public partial class Register : System.Web.UI.Page
     {
-        protected void CreateUser_Click(object sender, EventArgs e)
+        protected void Page_Load(object sender, EventArgs e)
         {
-            // Default UserStore constructor uses the default connection string named: DefaultConnection
-            var userStore = new UserStore<IdentityUser>();
-            var manager = new UserManager<IdentityUser>(userStore);
 
-            var user = new IdentityUser() { UserName = UserName.Text };
-            IdentityResult result = manager.Create(user, Password.Text);
-
-            if (result.Succeeded)
-            {
-                StatusMessage.Text = string.Format("User {0} was created successfully!", user.UserName);
-            }
-            else
-            {
-                StatusMessage.Text = result.Errors.FirstOrDefault();
-            }
         }
     }
 }
